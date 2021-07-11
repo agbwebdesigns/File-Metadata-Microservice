864359280e7e016a6b729343f169fab612504434
Adam Bennett <Adambennett5351@gmail.com>
2021-07-10 19:28:44 -0700

    init commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer=require('multer');
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,11 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res,next) =>  {
+  console.log(req.file.size);
+  const {filename,mimetype,size}=req.file;
+  res.send({name:filename,type:mimetype,size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
